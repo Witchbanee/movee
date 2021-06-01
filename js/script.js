1ff3a5128f9a1bee5244b15b0503ccf27f7b362d
Pavel <55561686+Witchbanee@users.noreply.github.com>
2021-06-01 16:33:10 +0300

    Add files via upload
    Доделал основной адаптив, ренейминг некоторых классов, поправка мелких косяков.
    Подготовка к добавлению js.

---
+++ js/script.js
@@ -0,0 +1,29 @@
+
+// let burger = document.querySelector(".nav-button");
+// let burgerLine = document.querySelector(".nav__line");
+// let burgerOpenMenu = document.querySelector(".navigation__main");
+// let body = document.querySelector("body");
+
+
+// burger.addEventListener("click", function (evt) {
+//     evt.preventDefault();
+//     burgerOpenMenu.classList.toggle("navigation--mobile");
+//     body.classList.toggle("position");
+//     burgerLine.classList.toggle("nav__line");
+//     burgerLine.classList.toggle("nav__line--open");
+// });
+
+// let burger = document.querySelector(".nav-button");
+// let burgerLine = document.querySelector(".nav__line");
+// let burgerOpenMenu = document.querySelector(".navigation__main");
+// let body = document.querySelector("body");
+
+// let watchAll = document.querySelector(".price__full");
+// let display = document.querySelector(".display-block");
+// let kartochka = document.querySelector(".price__item:not(:first-child)");
+
+
+// watchAll.addEventListener("click", function (evt) {
+//     evt.preventDefault();
+//     kartochka.classList.toggle("display-none");
+// });
